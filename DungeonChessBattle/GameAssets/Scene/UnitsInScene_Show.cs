95de5acb8368cd429c17890ba9db49583edb756f
DrMelt <ssxiayiz@outlook.com>
2026-08-02 13:46:14 +0800

    backup

--- DungeonChessBattle/GameAssets/Scene/UnitsInScene_Show.cs
+++ DungeonChessBattle/GameAssets/Scene/UnitsInScene_Show.cs
@@ -6,23 +6,11 @@ namespace DungeonChessBattle;
 public partial class UnitsInScene_Show : Node {
     readonly UnitsInScene unitsInSceneRes = new();
 
-    bool isPause = false;
-
     public UnitsInScene UnitsInSceneRes => unitsInSceneRes;
 
     public Array<UnitState> UnitsArr => unitsInSceneRes.UnitsArr;
 
-    public override void _Process(double delta) {
-        if (Input.IsActionJustPressed("Scene_Pause")) {
-            isPause = !isPause;
-        }
-    }
 
-    public override void _PhysicsProcess(double delta) {
-        if (!isPause) {
-            unitsInSceneRes.UpdateState(delta);
-        }
-    }
 
     public void AddUnitShow(UnitGameShow unitGameShow) {
         unitsInSceneRes.AddUnit(unitGameShow.UnitStateRec);
